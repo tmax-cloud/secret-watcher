5424c3b42d81694dd059cd8f10013fc631cfdbe3
sukhee_yun <sukhee_yun@tmax.co.kr>
2020-06-05 17:40:27 +0900

    MOD Constants

--- src/main/java/registrywatcher/Constants.java
+++ src/main/java/registrywatcher/Constants.java
@@ -1,29 +1,8 @@
 package registrywatcher;
 
 public class Constants {
-	public static final String WEBHOOK_NAMESPACE = "hypercloud-system";
-	public static final String ISSUER = "Tmax-ProAuth-WebHook";
-	public static final String ACCESS_TOKEN_SECRET_KEY = "Access-Token-Secret-Key";
-	public static final String REFRESH_TOKEN_SECRET_KEY = "Refresh-Token-Secret-Key";
-	
-	public static final String CUSTOM_OBJECT_GROUP = "tmax.co.kr";
-	public static final String CUSTOM_OBJECT_VERSION = "v1";
-	public static final String CUSTOM_OBJECT_PLURAL_USER = "users";
-	public static final String CUSTOM_OBJECT_PLURAL_TOKEN = "tokens";
-	public static final String CUSTOM_OBJECT_PLURAL_REGISTRY = "registries";
-	
-//	public static final String MASTER_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUbWF4LVByb0F1dGgtV2ViSG9vayIsImlkIjoid3ltaW4tdG1heC5jby5rciIsImV4cCI6MTU4MzEyMTQ5M30.hjvrlaLDFuSjchJKarGKbuWOuafhsuCQgBDo-pqsZvg";
-	public static final int ACCESS_TOKEN_EXP_TIME = 3600; // 1 hour
-	public static final int REFRESH_TOKEN_EXP_TIME = 604800; // 7 days
-	
-	public static final String CLAIM_USER_ID = "id";
-	public static final String CLAIM_TOKEN_ID = "tokenId";
-	
-
 	public static final String K8S_PREFIX = "hpcd-";
 	public static final String K8S_REGISTRY_PREFIX = "registry-";
-	public static final String REGISTRY_CPU_STRING = "0.2";
-	public static final String REGISTRY_MEMORY_STRING = "512Mi";
 
 	// OpenSSL Certificate Home Directory
 	public static final String OPENSSL_HOME_DIR = "/openssl";
@@ -41,10 +20,6 @@ public class Constants {
 	public static final String DOCKER_CONFIG_FILE = "config.json";
 	public static final String DOCKER_CONFIG_JSON_FILE = ".dockerconfigjson";
 	
-	// Shared Option
-	public static final int SHARE_ONLY_THIS_DOMAIN = 0;
-	public static final int SHARE_OTHER_DOMAINS = 1;
-	 
 	// Secret Type
 	public static final String K8S_SECRET_TYPE_DOCKER_CONFIG_JSON = "kubernetes.io/dockerconfigjson";
 
